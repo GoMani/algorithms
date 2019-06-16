476ab1bd6e5aedd13d1ad05919eceed89a76badd
Manikandan Perumal <manperumal@ebay.com>
2019-06-16 00:22:39 -0700

    selection sort

--- java/src/arrays/Sort.java
+++ java/src/arrays/Sort.java
@@ -27,13 +27,61 @@ class Sort {
             return i > j;
         return j > i;
     }
+/**
+ *  Selection Sort
+ *  Complexity O(nSquare)
+ * @param array
+ * @param asc
+ */
+    public void selectionSort(int[] array, boolean asc) {
+        if (array == null || array.length < 2)
+            return;
+        for (int j = 0; j < array.length - 1; j++) {
+            int current = array[j];
+            int i = j + 1;
+            int minOrMax = array[j];
+            int index = j;
+            while (i < array.length) {
+                if (comparator(min, array[i], asc)) {
+                    minOrMax = array[i];
+                    index = i;
+                }
+                i++;
+            }
+            array[j] = minOrMax;
+            array[index] = current;
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println("Sort");
-        int[] array = new int[] { 1, 2, 3, 5, 4 };
-        new Sort().insertionSort(array, false);
-        for (int i : array) {
-            System.out.println(i);
+        {
+            int[] array = new int[] { 1, 7, 3, 5, 4 };
+            System.out.println("insertion sort asc");
+            new Sort().insertionSort(array, true);
+            for (int i : array) {
+                System.out.println(i);
+            }
+            System.out.println("insertion sort desc");
+            new Sort().insertionSort(array, false);
+            for (int i : array) {
+                System.out.println(i);
+            }
         }
+        {
+            int[] array = new int[] { 1, 7, 3, 5, 4 };
+            System.out.println("Selection Sort asc");
+            new Sort().selectionSort(array, true);
+            for (int i : array) {
+                System.out.println(i);
+            }
+
+            new Sort().selectionSort(array, false);
+            System.out.println("Selection Sort desc");
+            for (int i : array) {
+                System.out.println(i);
+            }
+        }
+
     }
 }
--- python/src/arrays/Sort.py
+++ python/src/arrays/Sort.py
@@ -5,11 +5,9 @@ def insertionSort(arr,asc=True):
     if len(arr) <2 :
         return arr
     for j in range(1, len(arr)):
-        current= arr[j]
-        i= j-1
+        current,i= arr[j],j-1
         while i>=0 and comparator(arr[i],current,asc) :
-            arr[i+1]= arr[i]
-            i=i-1
+            arr[i+1],i= arr[i],i-1
         arr[i+1] =current
     return arr
 
@@ -18,9 +16,35 @@ def comparator(i,j,asc):
         return i > j
     return j > i
 
+def selectionSort(array,asc=True):
+    if len(array) <2 : 
+        return array
+    for j in range(0, len(array)-1):
+        current, minOrMax,index,i=array[j],array[j],j,j+1
+        while i < len(array):
+            if comparator(minOrMax,array[i],asc) :
+                minOrMax,index =array[i],i
+            i = i+1
+        array[j],array[index] =minOrMax,current
+    return array
+
+
+
 result= insertionSort( [4,2,5,1,3])
+print ("insertionSort asc")
 for i in result:
-    print i
+    print (i)
+print ("insertionSort desc")
 result= insertionSort( [4,2,5,1,3],False)
 for i in result:
-    print i
+    print (i)
+
+result= selectionSort( [4,2,5,1,3])
+print ("selectionSort asc")
+for i in result:
+    print (i)
+
+result= selectionSort( [4,2,5,1,3],False)
+print ("selectionSort desc")
+for i in result:
+    print (i)
