23a6cc85c8ac1bb264d912940ed989cb1514e810
Manikandan Perumal <mani.perumal78@gmail.com>
2019-06-30 12:49:22 -0700

    Update MinimumDistanceInGrid.java

--- java/src/arrays/MinimumDistanceInGrid.java
+++ java/src/arrays/MinimumDistanceInGrid.java
@@ -29,14 +29,9 @@ public class MinimumDistanceInGrid {
 		if (A == null || A.length < 2)
 			return 0;
 		int sum = 0;
-		int x = A[0];
-		int y = B[0];
 		for (int i = 1; i < A.length; i++) {
-			sum += Math.max(Math.abs(x - A[i]), Math.abs(y - B[i]));
-			x = A[i];
-			y = B[i];
+			sum += Math.max(Math.abs(A[i] - A[i-1]), Math.abs(B[i]- B[i-1]));
 		}
-
 		return sum;
 	}
 }
